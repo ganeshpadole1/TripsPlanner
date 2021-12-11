650620cec2a93a3b1959024baca684ed7e667502
ganesh padole <ganeshpadole55@gmail.com>
2021-12-11 14:31:45 +0530

    Populated data to show on tableview

--- TripsPlanner/Views/TripsViewController/TripsViewController.swift
+++ TripsPlanner/Views/TripsViewController/TripsViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class TripsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
         
+        TripFunctions.readTrips { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+extension TripsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        }
+        cell?.textLabel?.text = Data.tripModels[indexPath.row].title
+        return cell!
     }
 }
