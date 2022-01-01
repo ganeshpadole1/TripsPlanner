9627aadd3241bdabc02944439082af89188c1c21
ganesh padole <ganeshpadole55@gmail.com>
2022-01-01 17:52:27 +0530

    - Delete trip functionality.

--- TripsPlanner/Views/TripsViewController/TripsViewController.swift
+++ TripsPlanner/Views/TripsViewController/TripsViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class TripsViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addButton: FloatingActionButton!
     
@@ -17,7 +17,7 @@ class TripsViewController: UIViewController {
         
         view.backgroundColor = Theme.backgroundColor
         tableView.backgroundColor = .clear
-
+        
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -53,5 +53,24 @@ extension TripsViewController: UITableViewDataSource {
 extension TripsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 160
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let trip = Data.tripModels[indexPath.row]
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { (contextualAction, view, actionPerformed: @escaping (Bool) -> ()) in
+            
+            let alert = UIAlertController(title: "Delete Trip", message: "Are you sure you want to delete this trip: \(trip.title)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) {_ in
+                actionPerformed(false)
+            })
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { action in
+                TripFunctions.deleteTrip(index: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }))
+            self.present(alert, animated: true)
+        }
+        delete.image = UIImage(named: "delete")
+        return UISwipeActionsConfiguration(actions: [delete])
     }
 }
